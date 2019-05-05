95e580fd97a62b2ac4682a1243320ea05bf5d3da
Ayub2x <ayub2x@ahmeds-MacBook-Pro.local>
2019-05-05 17:02:27 -0500

    Friend-finder hw

---
+++ app/data/friend.js
@@ -0,0 +1,36 @@
+var friend = [
+  {
+    name: "Lauren London",
+    photo: "https://bookingagentinfo.com/wp-content/uploads/2016/03/Lauren-London-Contact-Information.jpg",
+    scores: [3, 4, 2, 4, 5, 1, 2, 5, 5, 1]
+  },
+
+
+  {
+    name: "Cam Newton",
+    photo: "https://sfbayview.com/wp-content/uploads/2016/01/Cam-Newton.jpg",
+    scores: [3, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+
+
+  {
+    name: "Megan Good",
+    photo: "https://www.aceshowbiz.com/images/photo/meagan_good.jpg",
+
+    scores: [5, 2, 3, 4, 2, 2, 3, 2, 4, 5]
+  },
+
+  {
+    name: "Tupac",
+    photo: "https://e3.365dm.com/18/07/768x432/skynews-tupac-shakur-rapper_4352100.jpg?20180703130024",
+
+    scores: [3, 4, 4, 2, 1, 1, 3, 2, 3, 5]
+  },
+
+
+
+];
+
+module.exports = friend;
+
+
